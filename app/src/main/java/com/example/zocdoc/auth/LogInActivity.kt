e8ef5fa2fc638baba03cf1fdf2ff3c65b3bf5589
Gaddar Kr Chaudhary <gaddarkumar7447@gmail.com>
2023-02-08 00:12:27 +0530

    LogIn acticity design

---
+++ app/src/main/java/com/example/zocdoc/auth/LogInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.zocdoc.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.zocdoc.R
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
